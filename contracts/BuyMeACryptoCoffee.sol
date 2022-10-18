93a4736967cf53710b31da7a18eb9ab3b6a94e80
ilya Smiyukha <ilya.smiyukha@touchcast.com>
2022-10-18 17:15:11 +0300

    init contract

---
+++ contracts/BuyMeACryptoCoffee.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract buyMeACryptoCoffee {
+    uint256 totalCoffee;
+
+    address payable public owner;
+
+    event NewCoffee(
+        address indexed from,
+        uint256 timestamp,
+        string message,
+        string name
+    );
+
+    constructor() payable {
+        console.log("This is contract");
+
+        owner = payable(msg.sender);
+    }
+
+    struct Coffee {
+        address giver;
+        string message;
+        string name;
+        uint256 timestamp;
+    }
+
+    Coffee[] coffee;
+
+    function getAllCoffee() public view returns (Coffee[] memory) {
+        return coffee;
+    }
+
+    function getTotalCoffee() public view returns (uint256) {
+        console.log("We have %d total coffee recieved ", totalCoffee);
+        return totalCoffee;
+    }
+
+    function buyCoffee(
+        string memory _message,
+        string memory _name,
+        uint256 _payAmount
+    ) public payable {
+
+        totalCoffee += 1;
+        console.log("%s has just sent a coffee!", msg.sender);
+
+        coffee.push(Coffee(msg.sender, _message, _name, block.timestamp));
+
+        (bool success, ) = owner.call{value: _payAmount}("");
+        require(success, "Failed to send money");
+
+        emit NewCoffee(msg.sender, block.timestamp, _message, _name);
+    }
+}
---
+++ scripts/run.js
@@ -0,0 +1,20 @@
+const main = async () => {
+
+  const coffeeContractFactory = await hre.ethers.getContractFactory('buyMeACryptoCoffee');
+  const coffeeContract = await coffeeContractFactory.deploy();
+
+  await coffeeContract.deployed();
+ console.log("Coffee Contract deployed to:", coffeeContract.address);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
